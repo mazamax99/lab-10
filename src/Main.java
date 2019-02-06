ac4514c81243b53bbf55d71021366ad6c36a1eab
mazamax99 <ziperziperziperziper@gmail.com>
2019-02-06 12:42:34 +0600

    2)Вывод описания исключения(любые исключения,кроме тех,которые из первых)

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,20 @@ public class Main {
         int c[]={1,2,3};
         int a=c[4];
     }
+    public static void expDesc(){
+        try{
+            int c[]=new int[-1];
+
+        }
+        catch(NegativeArraySizeException e){
+            System.out.println("Описание "+e+" (массив отрицательного размера)");
+
+        }
+    }
    public static void main(String args[]){
        //expZero();
-        expIndex();
+        //expIndex();
+       expDesc();
        }
     }
-// 1)Пример необрабатываемых исключений 2)Вывод описания исключения(любые исключения,кроме тех,которые из первых)
+// 1)Пример необрабатываемых исключений 2)Вывод описания исключения(любые исключения,кроме тех,которые из первых) 3)
