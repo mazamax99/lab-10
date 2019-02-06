3877c1af1dcecbeb8948020208b43b9546cc606f
mazamax99 <ziperziperziperziper@gmail.com>
2019-02-06 13:04:37 +0600

    3)Несколько catch(int0-5,float5-10,string10-15,все исключения(по нитервалам))

--- src/Main.java
+++ src/Main.java
@@ -14,13 +14,32 @@ public class Main {
         }
         catch(NegativeArraySizeException e){
             System.out.println("Описание "+e+" (массив отрицательного размера)");
-
         }
     }
-   public static void main(String args[]){
+    public static void expCatches(int a) throws Exception {
+        if(a>=0&&a<5) {
+            String first=" "+a;
+            throw new Exception(first);
+        }
+        if(a>=5&&a<10) {
+            float f=(float)a;
+            String second=" "+f;
+            throw new Exception(second);
+        }
+        if(a>=10&&a<15) {
+            String s[]={"Десять","Одиннадцать","Двенадцать","Тринадцать","Четырнадцать","Пятнадцать",};
+            throw new Exception(s[a-10]);
+        }
+        if(a>=15) {
+            throw new Exception("Unknown ERROR");
+        }
+    }
+   public static void main(String args[])throws Exception{
        //expZero();
         //expIndex();
-       expDesc();
+       //expDesc();
+       expCatches(20);
        }
     }
-// 1)Пример необрабатываемых исключений 2)Вывод описания исключения(любые исключения,кроме тех,которые из первых) 3)
+// 1)Пример необрабатываемых исключений 2)Вывод описания исключения(любые исключения,кроме тех,которые из первых)
+// 3)Несколько catch(int0-5,float5-10,string10-15,все исключения(по нитервалам))
