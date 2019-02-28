544713535af9e3a8382faa94f63abb4f63257df3
mazamax99 <ziperziperziperziper@gmail.com>
2019-02-28 12:03:16 +0600

    Add bat

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,55 @@
-﻿import java.util.Date;
+import java.util.Date;
 
 public class Main {
     //1
-    public static void expZero(){
-        int x=100;
-        int a=x/0;
+    public static void expZero() throws Exception {
+        try {
+            int x = 100;
+            int a = x / 0;
+        }
+        catch (ArithmeticException e){
+            System.out.println(e.toString());
+        }
     }
-    public static void expIndex(){
-        int c[]={1,2,3};
-        int a=c[4];
+    public static void expIndex() throws Exception {
+       try{ int c[]={1,2,3};
+        int a=c[4];}
+       catch (IndexOutOfBoundsException e){
+           System.out.println(e.toString());
+       }
     }
     //2
-    public static void expDesc(){
+    public static void expDesc() throws Exception {
         try{
             int c[]=new int[-1];
 
         }
         catch(NegativeArraySizeException e){
-            System.out.println("Описание "+e+" (массив отрицательного размера)");
+            System.out.println("Описание "+e.toString()+" (массив отрицательного размера)");
         }
+
     }
     //3
     public static void expCatches(int a) throws Exception {
-        if(a>=0&&a<5) {
-            String first=" "+a;
-            throw new Exception(first);
+        try {
         }
-        if(a>=5&&a<10) {
-            float f=(float)a;
-            String second=" "+f;
-            throw new Exception(second);
-        }
-        if(a>=10&&a<15) {
-            String s[]={"Десять","Одиннадцать","Двенадцать","Тринадцать","Четырнадцать","Пятнадцать",};
-            throw new Exception(s[a-10]);
-        }
-        if(a>=15) {
-            throw new Exception("Unknown ERROR");
+        catch(Exception e) {
+            if (a >= 0 && a < 5) {
+                String first = " " + a;
+                throw new Exception(first);
+            }
+            if (a >= 5 && a < 10) {
+                float f = (float) a;
+                String second = " " + f;
+                throw new Exception(second);
+            }
+            if (a >= 10 && a < 15) {
+                String s[] = {"Десять", "Одиннадцать", "Двенадцать", "Тринадцать", "Четырнадцать", "Пятнадцать",};
+                throw new Exception(s[a - 10]);
+            }
+            if (a >= 15) {
+                throw new Exception("Unknown ERROR");
+            }
         }
     }
     //4
@@ -66,4 +79,4 @@ public class Main {
     }
 // 1)Пример необрабатываемых исключений 2)Вывод описания исключения(любые исключения,кроме тех,которые из первых) 264 стр.
 // 3)Несколько catch(int0-5,float5-10,string10-15,все исключения(по нитервалам))
-// 4)Собственный подкласс исключений и передавать сообщение какой то int дентификатор, сообщение ошибки и дата время
+// 4)Собственный подкласс исключений и передавать сообщение какой то int дентификатор, сообщение ошибки и дата время123
